41e8fdeceed6bef047836b2821df8777a143c8ee
Adam Kvant <kvant003@umn.edu>
2024-02-01 11:04:48 -0600

    Making Doxyfile local

--- CardGamesCSharp/obj/Debug/net8.0/CardGamesCSharp.AssemblyInfo.cs
+++ CardGamesCSharp/obj/Debug/net8.0/CardGamesCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardGamesCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bde3cf039096280bf05db9b55bb745726d5dc56")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardGamesCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardGamesCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
